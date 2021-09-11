ea7eda58ebf772f195a01d1264bd09f3af0f2ec7
srija07 <79698217+srija07@users.noreply.github.com>
2021-09-11 13:04:24 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,81 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world;
+var backgroundImg;
+var hour;
+
+var bg = "sunrise.png";
+
+function preload() {
+    getBackgroundImg();
+    
+}
+
+function setup(){
+    var canvas = createCanvas(1200,700);
+    engine = Engine.create();
+    world = engine.world;
+
+}
+
+function draw(){
+    if(backgroundImg){
+        background(backgroundImg);
+    }
+    Engine.update(engine);
+
+    fill("black");
+    textSize(30);
+    
+    if(hour>=12){
+        text("Time : "+ hour%12 + " PM", 50,100);
+    }else if(hour==0){
+        text("Time : 12 AM",100,100);
+    }else{
+        text("Time : "+ hour%12 + " AM", 50,100);
+    }
+
+}
+
+async function getBackgroundImg(){
+    var response = await fetch("http://worldtimeapi.org/api/timezone/Asia/Kolkata")
+    console.log(response)
+    var responseJSON= await response.json()
+    console.log(responseJSON)
+    var dateTime= responseJSON.datetime
+    console.log(dateTime)
+    var hour = dateTime.slice(11,13)
+    console.log(hour)
+    if(hour>=6 && hour<=18){
+        backgroundImg=loadImage ("sprites/sunrise.png") 
+         }else{
+             backgroundImg=loadImage("sprites/sunset.png")
+         }
+        
+    // write code to fetch time from API
+    
+ 
+    //change the data in JSON format and store it in variable responseJSON
+    
+
+    
+    //fetch datetime from responseJSON
+    
+    
+
+    // slice the datetime to extract hour
+    
+
+    
+    if(hour>=0 && hour<18 ){
+        bg = "sunrise.png";
+    }
+    else{
+        bg="sunset.png"
+    }
+    
+    backgroundImg = loadImage(bg);
+}
